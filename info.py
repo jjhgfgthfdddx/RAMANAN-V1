eaedc2b94409198bed59b2a0a0cbb20b1693bbaf
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-15 10:53:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -27,7 +27,7 @@ PICS = (environ.get('PICS', 'https://telegra.ph/file/d47c44304c2219766033b.jpg')
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7425490417 1509123054 1499662579').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002397004421 -1002096796235').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002397004421 -1002096796235 -1002439612344').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
