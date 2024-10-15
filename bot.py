3c1dc686a82678d8d69e56b293c88abe0fc80b65
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-15 11:25:49 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,6 @@ from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
 from database.ia_filterdb import Media
 from database.users_chats_db import db
-from database.join_reqs import JoinReqs
 from info import *
 from utils import temp
 from aiohttp import web
