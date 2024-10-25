2d963074539c9cd7bca38476a25aa749f2889f6c
jjhgfgthfdddx <manafalthaf01@gmail.com>
2024-10-25 23:44:55 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -40,9 +40,9 @@ BTN = InlineKeyboardMarkup(
             ]
 )
 
-@Client.on_message(filters.group & filters.text & filters.incoming)
-async def give_filter(client, message):
-    k = await manual_filters(client, message)
+@Client.on_message((filters.group | filters.private) & filters.text & filters.incoming)
+async def give_filters(client, message):
+    k = await global_filters(client, message)    
     if k == False:
         await auto_filter(client, message)
 
